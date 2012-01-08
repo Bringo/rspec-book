b77d70b7cfe0109d5fa4e864f771f4de266cf360
Tim Trautmann <tim@foorama.com>
2012-01-08 11:00:29 -0800

    fixing duplicate guess issue

--- codebreaker/lib/codebreaker/marker.rb
+++ codebreaker/lib/codebreaker/marker.rb
@@ -4,6 +4,17 @@ class Marker
     @guess  = guess
   end
   
+  def total_match_count
+    secret = @secret.split('')
+    @guess.split('').inject(0) do |count, n|
+      count + (delete_first(secret, n) ? 1 : 0)
+    end
+  end
+  
+  def delete_first(code, n)
+    code.delete_at(code.index(n)) if code.index(n)
+  end
+  
   def exact_match_count
     (0..3).inject(0) do |count, index|
       count += exact_match?(index) ? 1 : 0
@@ -11,9 +22,7 @@ class Marker
   end
 
   def number_match_count
-    (0..3).inject(0) do |count, index|
-      count += number_match?(index) ? 1 : 0
-    end
+    total_match_count - exact_match_count
   end
 
   def exact_match?(index)
